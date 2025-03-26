6ef2c9210b56d29bc493769b2ae57419bc70dc19
Rohin Madhavan <rohinmadhavan96@gmail.com>
2025-03-26 20:48:53 +0000

    Create Keys.swift

---
+++ Pixel Scape/Base/Keys.swift
@@ -0,0 +1,12 @@
+//
+//  Keys.swift
+//  Pixel Scape
+//
+//  Created by Rohin Madhavan on 26/03/2025.
+//
+
+import Foundation
+
+struct Keys {
+    static let pixelAPIKey = "GpXf7H0tEtl4CXri8Ft2FxZgKlIbaiuth56DwDjV0XEmt8HGZQCUEnXB"
+}
