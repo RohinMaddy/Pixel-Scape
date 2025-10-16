d7103057693cb816c6e4b5901b7c71a0c94bd43d
Rohin Madhavan <rohinmadhavan96@gmail.com>
2025-10-16 14:07:38 +0100

    add test target

---
+++ Pixel ScapeTests/Pixel_ScapeTests.swift
@@ -0,0 +1,16 @@
+//
+//  Pixel_ScapeTests.swift
+//  Pixel ScapeTests
+//
+//  Created by Rohin Madhavan on 16/10/2025.
+//
+
+import Testing
+
+struct Pixel_ScapeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
